92e885be598045ad3bf27adb028a16e885a0b069
injoon <injoon5@naver.com>
2021-05-22 05:28:20 +0000

    More font & color support

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from textimg import gen
 
 app = Flask('Discord Text Image')
 gen(" ")
 
+fonts = ["nanum", "lotte", "gungseo", "plexsans", "gugi", "noto", "hannuri", "krfr-type", "seoulnamsan", "slei", "spoqa"]
+colors = ["original", "red", "green", "yellow", "blue", "pink", "grey"]
+
 @app.errorhandler(404)
 def page_not_found(error):
-  gen("!")
-  return send_file('pil_text.png', download_name='Fatal error!!!.jpg')
+  gen("404 Not Found")
+  return send_file('pil_text.png', download_name='Fatal error!!!.png')
+@app.route('/g/<hh>')
+def moved(hh):
+  gen("g를 빼고 요청해주세요.")
+  return send_file('pil_text.png', download_name="checkaddress.png")
+@app.route('/<string:text>')
 
-@app.route('/g/<text>', strict_slashes=False)
+@app.route('/<string:text>')
+@app.route('/', defaults={ "text": "텍스트 확인" })
 def gen_page(text):
-  if len(text) > 1:
-    gen("!")
-    return send_file('pil_text.png', download_name='Fatal error!!!.jpg')
-  elif len(text) < 1:
-    gen(" ")
-    return send_file('pil_text.png', download_name='empty.png')
-  gen(text)
+  #text = request.path.split("/")[2]
+  font = "nanum"
+  color = "original"
+  if "font" in request.args.to_dict():
+    font = request.args.to_dict()["font"]
+  if "color" in request.args.to_dict():
+    color = request.args.to_dict()["color"] 
+  
+  if (color not in colors or font not in fonts) or (color not in colors and font not in fonts):
+    gen("요청이 잘못되었습니다. ")
+    return send_file('pil_text.png', download_name='Fatal error!!!.png')
+  if len(text) > 10:
+    gen("10자 까지만 허용")
+    return send_file('pil_text.png', download_name='Fatal error!!!.png')
+
+  gen(text, color, font)
   return send_file('pil_text.png', download_name='Created with pil-discord-txt.png')
 
 app.run(host='0.0.0.0', port=8080)
--- textimg.py
+++ textimg.py
@@ -3,13 +3,17 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
-def gen(text):
+fontDict = {"nanum": "font/NanumGothic-Bold.ttf", "lotte": "font/12롯데마트드림Medium.ttf", "gungseo": "font/hy궁서-yoond1004.ttf", "plexsans": "font/IBMPlexSansKR-Regular.ttf", "gugi": "font/Gugi-Regular.ttf", "hannuri": "font/HangeulNuriR.ttf", "krfr-type": "font/KoreanFrenchTypewriter.ttf", "seoulnamsan": "font/SeoulNamsanM.ttf", "slei": "font/SLEIGothicTTF.ttf", "spoqa": "font/SpoqaHanSansNeo-Regular.ttf"}
+colorDict = {"original": {"stroke": (50,53,59,255), "color": (250,250,250,255)}, "red": {"stroke": (255, 0, 0,0), "color": (255, 0, 0)}, "green": {"stroke": (60, 179, 113,0), "color": (60, 179, 113)}, "yellow": {"stroke": (255, 165, 0,0), "color": (255, 165, 0)}, "blue": {"stroke": (0, 0, 255,0), "color": (0, 0, 255)}, "pink": {"stroke": (238, 130, 238,0), "color": (238, 130, 238)}, "grey": {"stroke": (120, 120, 120,0), "color": (120, 120, 120)}}
+def gen(text, color="original", font="nanum"):
   # font = ImageFont.truetype("Arial-Bold.ttf",14)
-  font = ImageFont.truetype("NanumGothic-Bold.ttf",42)
-  img=Image.new("RGBA", (50,50),(255,255,255,0))
+  font = ImageFont.truetype(fontDict[font],42)
+  x = font.getsize(text)[0] + 2
+  y = font.getsize(text)[1] + 2
+  img=Image.new("RGBA", (x,y),(255,255,255,0))
   draw = ImageDraw.Draw(img)
   # Black: (50,53,59,255)
   # White: (250,250,250,255)
-  draw.text((25, 25),text,(250,250,250,255),font=font, anchor="mm", stroke_width=3, stroke_fill=(50,53,59,255))
+  draw.text((x, y/2), text, colorDict[color]["color"], font=font, anchor="rm", stroke_width=3, stroke_fill=colorDict[color]["stroke"])
   draw = ImageDraw.Draw(img)
   img.save("pil_text.png")
