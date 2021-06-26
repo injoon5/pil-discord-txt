57ddcdf33ca2d0b429cb2607bd53c4cadf99a50b
injoon <injoon5@naver.com>
2021-06-26 17:05:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def gen_page(text):
   gen(text, color, font)
   return send_file('pil_text.png', download_name='Created with pil-discord-txt.png')
 
-app.run(host='0.0.0.0', port=8080)
+app.run(port=os.getenv("PORT", default=5000))
